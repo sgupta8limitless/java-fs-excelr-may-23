200a882e7ab457f727f2677b165f3cd096e45c6e
SAURABH <sgupta8mz@gmail.com>
2023-08-24 22:19:33 +0530

    24th august commit

---
+++ javascript-fundamentals/script.js
@@ -0,0 +1,112 @@
+
+// let nums = [23,45,67,89];
+
+// console.log(nums);
+
+// Objects 
+
+// let student1 = {
+//     name:"Saurabh",
+//     age:25,
+//     city:"Mumbai"
+// };
+
+// let student2 = {
+//     name:"Aqib",
+//     age:22,
+//     city:"Baramulla"
+// };
+
+// console.log(student2.name);
+
+
+// let students = [
+
+//     {name:"Saurabh",age:24,city:"Mumbai"},
+//     {name:"Aqib",age:34,city:"Baramulla",state:"Kashmir"},
+//     {name:"Vikashini",age:18,city:"Indore"}
+
+// ];
+
+// console.log(students);
+
+// // console.log(students[0].name);
+// // console.log(students[1].name);
+// // console.log(students[2].name);
+
+// for(let i=0;i<students.length;i++)
+// {
+//     console.log(students[i].age);
+// }
+
+
+
+// function doSomething(name,age,marks,animal)
+// {
+//     // console.log("The name is "+name+" and the age is "+age);
+//     console.log(`the name is ${name} and the age is ${age}`);
+//     console.log(marks);
+//     console.log(animal);
+// }
+
+
+// let m = [23,45,67,89];
+
+// doSomething("Saurabh",23,m,{name:"Cheetah",kingdom:"animal"});
+
+// function add(num1,num2)
+// {
+//     let res = num1+num2;
+//     return res;
+
+    
+// }
+
+
+// let a = add(23,45);
+
+// console.log(add(56,78));
+
+
+// function doSomething(dummy)
+// {
+//     console.log("do something code");
+//     // console.log(dummy);
+//     dummy("Saurabh");
+// }
+
+// doSomething(function (name){
+//     console.log("som func"+name);
+// });
+
+
+// function add()
+// {
+//     console.log("hello");
+// }
+
+// let add = function()
+// {
+//     console.log("hello");
+// }
+
+// add();
+
+
+// function add(name)
+// {
+//     console.log("hello "+name);
+// }
+
+
+// arrow functions 
+// if line of code is 1 no need for block
+// if its is one line and if its return statement no need to mention return
+
+let add = (num1,num2) => num1+num2;
+
+
+console.log(add(24,56));
+
+
+
